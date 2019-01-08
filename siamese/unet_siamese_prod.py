1100859a067e38fe622c422655454f4183f3ab4e
sagarverma <sagar15056@iiitd.ac.in>
2019-01-08 19:36:11 +0100

    restrucure and readability started

--- siamese/unet_siamese_prod.py
+++ siamese/unet_siamese_prod.py
@@ -25,34 +25,52 @@ def w(v):
         return v.cuda(DEVICE)
     return v
 
+input_size = 64
+stride = 16
+num_workers = 4
+gpu_id = 0
+augmentation = True
+train_city_split = [0,1]
+
 epochs = 200
 batch_size = 64
-input_size = 64
 layers = 6
 lr = 0.01
 init_filters = 32
+
 loss_func = 'focal'
 weight_factor = 2
-# bands = ['B01','B02', 'B03', 'B04','B05','B06','B07','B08','B8A','B09','B10','B11','B12']
-bands = ['B02', 'B03', 'B04','B08']
+
+bands = ['B01','B02', 'B03', 'B04','B05','B06','B07','B08','B8A','B09','B10','B11','B12']
+num_channels = len(bands) #how to pass bands in param
+fusion_method = 'mul' #cat, add, mul, sub, div
+
 data_dir = '../../datasets/onera/'
 weight_path = '../../weights/onera/unet_siamese_prod_relu_inp64_4band_2dates_focal_6layers_1e-3lr_hm_cnc_all_14_cities.pt'
 train_csv = '../../datasets/onera/train_64x64.csv'
 test_csv = '../../datasets/onera/test_64x64.csv'
+log_path = '../../logs/onera/'
 
-net = w(UNetClassify(layers=layers, init_filters=init_filters, num_channels=4, fusion_method='mul'))
-# weights = torch.load('../../weights/onera/unet_siamese_prod_relu_inp64_13band_2dates_focal_7layers_hm_cnc_all_14_cities.pt')
-# net.load_state_dict(weights)
+###############################
+#1. CHANGE PRECISION, Recall, F1Score, IOU in metrics_and_losses
+#2. Data augmentation in dataloader
+#3. sampling code in dataloader
+#4. log creator code
+#5. args handling
+###############################
+
+exit()
+full_load = full_onera_loader(data_dir, bands)
+train_dataset = OneraPreloader(data_dir , train_csv, input_size, full_load, onera_siamese_loader)
+train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+
+test_dataset = OneraPreloader(data_dir , test_csv, input_size, full_load, onera_siamese_loader)
+test = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+
+net = w(UNetClassify(layers=layers, init_filters=init_filters, num_channels=num_channels, fusion_method=fusion_method))
 
 criterion = get_loss(loss_func, weight_factor)
 optimizer = optim.Adam(net.parameters(), lr=lr)
-
-full_load = full_onera_loader(data_dir, bands)
-train_dataset = OneraPreloader(data_dir , train_csv, input_size, full_load, onera_siamese_loader)
-train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, drop_last=True)
-
-test_dataset = OneraPreloader(data_dir , test_csv, input_size, full_load, onera_siamese_loader)
-test = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4, drop_last=True)
 
 best_iou = -1.0
 best_net_dict = None
@@ -66,7 +84,7 @@ for epoch in tqdm(range(epochs)):
         batch_img1 = w(autograd.Variable(batch_img1))
         batch_img2 = w(autograd.Variable(batch_img2))
         labels = w(autograd.Variable(labels))
-        
+
         optimizer.zero_grad()
         output = net(batch_img1, batch_img2)
         loss = criterion(output, labels.view(-1,1,input_size,input_size).float())
