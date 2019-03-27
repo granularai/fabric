7e4ea30831deb88c5810efe6e5f83310b4beeede
sagarverma <sagar15056@iiitd.ac.in>
2019-03-27 15:57:01 +0100

    lulc only

--- models/bidate_model.py
+++ models/bidate_model.py
@@ -10,7 +10,7 @@ class BiDateNet(nn.Module):
         self.down2 = down(128, 256)
         self.down3 = down(256, 512)
         self.down4 = down(512, 512)
-        
+
         self.up1 = up(1024, 256)
         self.up2 = up(512, 128)
         self.up3 = up(256, 64)
@@ -23,13 +23,13 @@ class BiDateNet(nn.Module):
         x3_d1 = self.down2(x2_d1)
         x4_d1 = self.down3(x3_d1)
         x5_d1 = self.down4(x4_d1)
-        
+
         x1_d2 = self.inc(x_d2)
         x2_d2 = self.down1(x1_d2)
         x3_d2 = self.down2(x2_d2)
         x4_d2 = self.down3(x3_d2)
         x5_d2 = self.down4(x4_d2)
-        
+
         x = self.up1(torch.relu(x5_d2 * x5_d1), torch.relu(x4_d2 * x4_d1))
         x = self.up2(x, torch.relu(x3_d2 * x3_d1))
         x = self.up3(x, torch.relu(x2_d2 * x2_d1))
@@ -45,7 +45,7 @@ class BiDateLULCNet(nn.Module):
         self.down2 = down(128, 256)
         self.down3 = down(256, 512)
         self.down4 = down(512, 512)
-        
+
         self.up1 = up(1024, 256)
         self.up2 = up(512, 128)
         self.up3 = up(256, 64)
@@ -59,13 +59,13 @@ class BiDateLULCNet(nn.Module):
         x3_d1 = self.down2(x2_d1)
         x4_d1 = self.down3(x3_d1)
         x5_d1 = self.down4(x4_d1)
-        
+
         x1_d2 = self.inc(x_d2)
         x2_d2 = self.down1(x1_d2)
         x3_d2 = self.down2(x2_d2)
         x4_d2 = self.down3(x3_d2)
         x5_d2 = self.down4(x4_d2)
-        
+
         x = self.up1(torch.relu(x5_d2 * x5_d1), torch.relu(x4_d2 * x4_d1))
         x = self.up2(x, torch.relu(x3_d2 * x3_d1))
         x = self.up3(x, torch.relu(x2_d2 * x2_d1))
@@ -73,3 +73,31 @@ class BiDateLULCNet(nn.Module):
         x_cd = self.outc(x)
         x_lulc = self.outc_lulc(x)
         return x_cd, x_lulc
+
+
+class LULCNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(LULCNet, self).__init__()
+        self.inc = inconv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.up3 = up(256, 64)
+        self.up4 = up(128, 64)
+        self.outc = outconv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        return x
