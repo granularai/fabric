90f1ad5b2a101205701204698b1ff5107f18cb7f
Siddharth Gupta <sid@granular.ai>
2020-09-22 17:26:19 -0400

    removing signature

--- train.py
+++ train.py
@@ -70,7 +70,6 @@ model_cls = BiDateNet
 model_module = getmodule(model_cls).__name__
 model_path = os.path.relpath(getmodule(model_cls).__file__)
 model_name = model_cls.__name__
-model_signature = signature(model_cls)
 
 print('logging manually')
 print(
@@ -79,14 +78,12 @@ print(
         model_module=getmodule(model_cls).__name__,
         model_path=os.path.relpath(getmodule(model_cls).__file__),
         model_name=model_cls.__name__,
-        model_signature=signature(model_cls),
     ))
 experiment.log_inputs(
     foo="bar",
     model_path=model_path,
     model_name=model_name,
     model_module=model_module,
-    model_signature=model_signature,
     model_args=dict(n_channels=4, n_classes=1),
 )
 
